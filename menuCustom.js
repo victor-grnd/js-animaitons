5681a46aeb0a93d0039d8ae9866c4843a963ee09
Victor <garandeauvictor@gmail.com>
2026-01-07 13:32:00 +0100

    corect menu toggle for custom menu

--- menuCustom.js
+++ menuCustom.js
@@ -1,17 +1,10 @@
 const openButton = document.querySelector(".hero_script_open");
 const menuWrap = document.querySelector(".hero_script_menu_wrap");
-let isOpening = false;
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
+let isOpen = false;
 
 openButton.addEventListener("click", () => {
-  if (isOpening) return;
-  isOpening = true;
-  menuWrap.classList.toggle("is-open");
-  sleep(300);
-  isOpening = false;
+  console.log("clicked");
+  const yValue = isOpen ? "-100%" : "0%";
+  isOpen = !isOpen;
+  menuWrap.style.transform = `translateY(${yValue})`;
 });
