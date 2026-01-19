806c6ab0afb7b2db4342a4a38a8449489cf9db2b
Victor <garandeauvictor@gmail.com>
2026-01-19 16:08:33 +0100

    update the rvlt code animation

--- revolut_hero_animation/rvlt.js
+++ revolut_hero_animation/rvlt.js
@@ -1,41 +1,99 @@
 const sectionTwo = document.querySelector(".rvlt_layer-two_wrap");
 const womanCard = document.querySelector(".rvlt_layer-two_bg");
-let animationWasPlayed = false;
-let frameId;
+let animationIsPlaying = false;
+let rvltTl;
+let ticking = false;
+let currentScroll;
+let lastScroll;
+let scrollDirectionIsForward = true;
+let nextAnimationMustBeReversed = false;
 
-let scrollValue;
+function getScrollDirection() {
+  if (!lastScroll) lastScroll = 0;
+  console.log(lastScroll);
+  currentScroll = window.scrollY;
+  console.log(currentScroll);
+  if (currentScroll > lastScroll) {
+    scrollDirectionIsForward = true;
+  } else {
+    scrollDirectionIsForward = false;
+  }
+  lastScroll = currentScroll;
+}
 
-function getScroll() {
-  scrollValue = window.scrollY;
-  frameId = requestAnimationFrame(getScroll);
-  if (scrollValue > 50) {
-    console.log("50px reach");
-    cancelAnimationFrame(frameId);
-    if (!animationWasPlayed) startAnimation();
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    requestAnimationFrame(() => {
+      getScrollDirection();
+      getScrollThereshold();
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
+
+function getScrollThereshold() {
+  if (
+    currentScroll > 50 &&
+    scrollDirectionIsForward &&
+    !nextAnimationMustBeReversed
+  ) {
+    startAnimation();
+  } else if (
+    currentScroll < 5 &&
+    !scrollDirectionIsForward &&
+    nextAnimationMustBeReversed
+  ) {
+    startAnimation();
   }
 }
 
-function startAnimation() {
-  const rvltTl = gsap.timeline({
+function initRevolutTimeline() {
+  rvltTl = gsap.timeline({
     paused: true,
+    onStart: () => {
+      animationIsPlaying = true;
+    },
     onComplete: () => {
-      animationWasPlayed = true;
-      console.log("animation finished");
+      animationIsPlaying = false;
+      nextAnimationMustBeReversed = true;
+    },
+    onReverseComplete: () => {
+      animationIsPlaying = false;
+      nextAnimationMustBeReversed = false;
     },
   });
 
-  rvltTl.to(sectionTwo, {
-    clipPath: "inset(0 0 0 0)",
-    duration: 1,
-    ease: "sine.out",
-  });
-  rvltTl.to(womanCard, {
-    transform: "scale(0.5)",
-    duration: 1,
-    ease: "sine.out",
-  });
-
-  rvltTl.play();
+  rvltTl.fromTo(
+    sectionTwo,
+    {
+      clipPath: "inset(20vh 34vw 15vh 34vw round 2rem)",
+    },
+    {
+      clipPath: "inset(0 0 0 0)",
+      duration: 1,
+      ease: "sine.out",
+    },
+    0,
+  );
+  rvltTl.fromTo(
+    womanCard,
+    {
+      scale: 1,
+    },
+    {
+      scale: 0.5,
+      duration: 1,
+      ease: "sine.out",
+    },
+    0,
+  );
 }
 
-getScroll();
+function startAnimation() {
+  if (animationIsPlaying) return;
+
+  if (scrollDirectionIsForward) rvltTl.play();
+  else rvltTl.reverse();
+}
+initRevolutTimeline();
