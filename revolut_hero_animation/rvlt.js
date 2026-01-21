9f85fd1d8a7512a0e18c54ca4a23f9a4c1f6e318
Victor <garandeauvictor@gmail.com>
2026-01-21 18:28:49 +0100

    change the code of rrvlt animation with mask size and gsap flip

--- revolut_hero_animation/rvlt.js
+++ revolut_hero_animation/rvlt.js
@@ -1,21 +1,20 @@
 const sectionTwo = document.querySelector(".rvlt_layer-two_wrap");
 const womanCard = document.querySelector(".rvlt_layer-two_bg");
-const whiteLayer = document.querySelector(".rvlt_layer-two_white");
-
+const cardsWrapper = document.querySelector(".u-grid.cards_wrapper");
 let animationIsPlaying = false;
 let rvltTl;
 let ticking = false;
-let currentScroll;
-let lastScroll;
+let currentScroll = 0;
+let lastScroll = 0;
 let scrollDirectionIsForward = true;
 let nextAnimationMustBeReversed = false;
+let isInitialized = false;
 
 function getScrollDirection() {
-  if (!lastScroll) lastScroll = 0;
   currentScroll = window.scrollY;
   if (currentScroll > lastScroll) {
     scrollDirectionIsForward = true;
-  } else {
+  } else if (currentScroll < lastScroll) {
     scrollDirectionIsForward = false;
   }
   lastScroll = currentScroll;
@@ -25,79 +24,96 @@ window.addEventListener("scroll", () => {
   if (!ticking) {
     requestAnimationFrame(() => {
       getScrollDirection();
-      getScrollThereshold();
+      getScrollThreshold();
       ticking = false;
     });
     ticking = true;
   }
 });
 
-function getScrollThereshold() {
+function getScrollThreshold() {
   if (
     currentScroll > 50 &&
     scrollDirectionIsForward &&
     !nextAnimationMustBeReversed
   ) {
-    startAnimation();
+    initRevolutTimeline();
   } else if (
     currentScroll < 5 &&
     !scrollDirectionIsForward &&
     nextAnimationMustBeReversed
   ) {
-    startAnimation();
+    initRevolutTimeline();
   }
 }
 
 function initRevolutTimeline() {
-  if (!whiteLayer || !womanCard || !sectionTwo) {
+  if (!womanCard || !sectionTwo || !cardsWrapper) {
     return;
   }
-  rvltTl = gsap.timeline({
-    paused: true,
-    onStart: () => {
-      animationIsPlaying = true;
-      whiteLayer.style.display = "none";
-    },
-    onComplete: () => {
-      animationIsPlaying = false;
-      nextAnimationMustBeReversed = true;
-    },
-    onReverseComplete: () => {
-      animationIsPlaying = false;
-      nextAnimationMustBeReversed = false;
-      whiteLayer.style.display = "block";
-    },
-  });
-  rvltTl.fromTo(
-    sectionTwo,
-    {
-      clipPath: "inset(20vh 34vw 15vh 34vw round 2rem)",
-    },
-    {
-      clipPath: "inset(0% 0% 0% 0%);",
-      duration: 0.5,
-    },
-    0,
-  );
-
-  rvltTl.fromTo(
-    womanCard,
-    {
-      scale: 1,
-    },
-    {
-      scale: 0.5,
-      duration: 0.7,
-      ease: "sine.out",
-    },
-    0,
-  );
-}
-
-function startAnimation() {
   if (animationIsPlaying) return;
+  if (!isInitialized) {
+    rvltTl = gsap.timeline({
+      paused: true,
+      onStart: () => {
+        animationIsPlaying = true;
+        /*const initialState = Flip.getState(womanCard);
 
-  if (scrollDirectionIsForward) rvltTl.play();
-  else rvltTl.reverse();
+        if (scrollDirectionIsForward) {
+          womanCard.classList.add("is-card");
+          const firstCard = cardsWrapper.firstElementChild;
+          if (firstCard) {
+            firstCard.after(womanCard);
+          } else {
+            cardsWrapper.appendChild(womanCard);
+          }
+
+          Flip.from(initialState, {
+            duration: 1,
+            ease: "power1.in",
+          })
+        }*/
+      },
+      onComplete: () => {
+        animationIsPlaying = false;
+        nextAnimationMustBeReversed = true;
+      },
+      onReverseComplete: () => {
+        animationIsPlaying = false;
+        nextAnimationMustBeReversed = false;
+        /*const reverseInitialState = Flip.getState(womanCard);
+
+        womanCard.classList.remove("is-card");
+        sectionTwo.appendChild(womanCard);
+
+        // Flip back to original position
+        Flip.from(reverseInitialState, {
+          duration: 1,
+          ease: "power1.out",
+        });*/
+      },
+    });
+
+    // Mask animation
+    rvltTl.fromTo(
+      sectionTwo,
+      {
+        maskSize: "45vh 70vh",
+      },
+      {
+        maskSize: "100% 100%",
+        duration: 0.5,
+        ease: "power1.in",
+      },
+      0,
+    );
+
+    isInitialized = true;
+  }
+
+  if (scrollDirectionIsForward) {
+    rvltTl.play();
+  } else {
+    rvltTl.reverse();
+  }
 }
-initRevolutTimeline();
